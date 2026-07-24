f38794d553b36e06baac067fb3052461f7306fe4
Shivam Davande <shivamdavande348@gmail.com>
2026-07-25 00:34:41 +0530

    first

---
+++ intro.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+
+class Hero {
+
+    int health;
+};
+
+int main() {
+
+    Hero h1;
+
+    cout << "size of hero is :- " << sizeof(h1) << endl;
+
+    return 0;
+}
